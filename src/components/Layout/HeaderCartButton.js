bb14b77b0bf9f8cfbf3581462c68ba715bf1b496
Laxita <86618594+laxitabaheti@users.noreply.github.com>
2022-02-09 00:06:06 +0530

    last Commit

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,16 +2,35 @@ import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 // import CartProvider from "../Store/CartProvider";
 import CartContext from "../Store/CartContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+
+  const [bumpisthere, SetBumpIsThere] = useState(false);
 
   const numberOfCartItems = cartCtx.items.reduce((numCurrent, item) => {
     return numCurrent + item.amount;
   }, 0);
 
+  const { items } = cartCtx;
+
+  const buttonClasses = `${styles.button} ${bumpisthere ? styles.bump : " "}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    SetBumpIsThere(true);
+    const Indetifier = setTimeout(() => {
+      SetBumpIsThere(false);
+    }, 300);
+    return () => {
+      clearTimeout(Indetifier);
+    };
+  }, [items]);
+
   return (
-    <button onClick={props.onClick} className={styles.button}>
+    <button onClick={props.onClick} className={buttonClasses}>
       <span className={styles.cartIcon}>
         <CartIcon />
       </span>
